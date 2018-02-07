5f5ddf87779f4c5bfc63902bc27decd32271ce47
Zero Day Hack Security <36053992+zerodayhack0@users.noreply.github.com>
2018-02-07 16:08:18 +0330

    Update Show ip WebSite.py

--- Show ip WebSite.py
+++ Show ip WebSite.py
@@ -1,13 +1,16 @@
 print """
-
 #	Telegram : @zero_day_hack  #
 #	Zero Day Hack Security     #
-
 """
 
 
 import socket
 
-name_site = raw_input("Enter Address Web Site : ")
-
-print(socket.gethostbyname(name_site))
+while True:
+      
+      name_site = raw_input("Enter Address Web Site : ")
+      try:
+            print(socket.gethostbyname(name_site))
+            print "\n"
+      except:
+            print "Url Error !"
